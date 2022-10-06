05d98dd796451a089682e7f64e4463cd0f1c0fdf
Xenolia <burak@xenolia.com>
2022-10-06 13:57:03 +0300

    Joystick added

--- Assets/Burakhrk/Scripts/BattleTrigger.cs
+++ Assets/Burakhrk/Scripts/BattleTrigger.cs
@@ -5,12 +5,17 @@ using UnityEngine;
 public class BattleTrigger : MonoBehaviour
 {
     [SerializeField] BattleManager battleManager;
+    private void Awake()
+    {
+        if (!battleManager)
+            battleManager = GameManager.Instance.gameObject.GetComponent<BattleManager>();
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-           other.gameObject.GetComponent<CombatScript>().enabled = true;
-            battleManager.OnBattleInvoke();
+          other.gameObject.GetComponent<CombatScript>().enabled = true;
+           battleManager.OnBattleInvoke();
         }
     }
 }
