21b57fa66e341abfc128cded12eb4c7bd0b320a7
Xenolia <burak@xenolia.com>
2022-10-14 16:44:09 +0300

    Fog Bux Fixing

---
+++ Assets/FogTransformSetter.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FogTransformSetter : MonoBehaviour
+{
+    [SerializeField] GameObject player;
+    private Vector3 fogPos;
+    private void Awake()
+    {
+        if (!player)
+            player = GameObject.FindGameObjectWithTag("Player");
+
+        fogPos = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+    }
+    private void LateUpdate()
+    {
+        fogPos = new Vector3(fogPos.x, player.transform.position.y, fogPos.z);
+    }
+}
